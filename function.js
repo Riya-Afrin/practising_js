39944e53e16eabd53e1bfd11e94713e7dfd2d619
Riya-Afrin <riyaafronesa@gmail.com>
2023-01-14 15:59:08 +0600

    added function

--- function.js
+++ function.js
@@ -1,6 +1,24 @@
+/*
 function count(num) {
     var divide = num / 5;
     return divide;
   }
   var result = count(50);
   console.log(result);
+*/
+
+//!getting the even number and multiplying it with 2.
+function evenDouble(element) {
+  if (element % 2 == 0){
+    console.log(element*2 , 'is a even number')
+  }
+  else{
+    console.log(element , 'is an odd number')
+  }
+}
+
+num = [5, 8, 15, 48 , 2, 3];
+for (let i = 0; i < num.length; i++) {
+  const element = num[i];
+  evenDouble(element);
+}
